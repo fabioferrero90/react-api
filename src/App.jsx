daa501d351420b9757f3b2bfc0e325c8d54f787b
fabioferrero90 <fabio.ff90@gmail.com>
2025-01-22 13:36:00 +0100

    feat: show post received from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 function App() {
-  <>
-    App
-  </>
+
+  const base_endpoint= import.meta.env.VITE_API_URL
+  const [posts, setPosts] = useState([])
+
+
+  const fetchData = () => (
+    axios.get(`${base_endpoint}/posts`)
+      .then(res => (
+        console.log(res.data),
+        setPosts(res.data)
+      ))
+  )
+
+  const handleImageError = (e) => {
+    e.target.src = 'https://placehold.co/600x400';
+  }
+
+  useEffect(() =>{
+    fetchData()
+  }, [])
+
+  return(
+    <>
+      <h1 className="mt-5 text-center">Elenco dei Post</h1>
+      <div className="cardbox container mt-5">
+        <div className="row">
+          {posts.map(post => (
+              <div className="card p-3 col-6" key={post.id}>
+                <img src={`${import.meta.env.VITE_API_URL}${post.image}`} className="card-img-top" alt={post.title} onError={handleImageError}/>
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <p className="card-text">{post.content}</p>
+                  <a href="#" className="btn btn-danger">Elimina</a>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
